f907e712b93a95e245d890242dc20ce195587444
tagaism <i77tag@gmail.com>
2012-06-08 00:53:10 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taiko::Application.config.secret_token = '542e65d873aa5eec30007b8fd742f4f0ca1d84e95338585b5edd48e8308105aad1eb08e9c73738aed217e5fe5d50a4a0c71e52c50f0b965febf59ba7b3ad82e4'
